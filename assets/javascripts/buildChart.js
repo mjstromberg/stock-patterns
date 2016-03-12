1f6ca79ced9d556a040b14a8e074f417ba838188
michaeljstromberg <michaeljstromberg@gmail.com>
2016-03-12 01:33:49 -0500

    Squashed bugs, loaded S&P at start, and refactored

--- assets/javascripts/buildChart.js
+++ assets/javascripts/buildChart.js
@@ -1,75 +1,117 @@
+/* ======================================================================== */
+/* DEFINE FUNCTIONS */
+/* ======================================================================== */
+
 var httpRequest;
 
 // Define function to get ticker symbol and make http reqest
-var getStock = function() {
+var getStockURL = function() {
   var stockName = document.getElementById('tickerTextbox').value.toString().toUpperCase();
   var stockURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?api_key=fyWKH12nMF4VuWFaXARN&limit=100';
+
+  return stockURL;
+};
+
+
+// Define function to format received data
+var formatData = function(chartType, dataset) {
+  var reformatStockData = dataset.data.reduce(function(result, dayArrayData) {
+    // result = [ adjOpen, adjHigh, adjLow, adjClose, date]
+    result[4].unshift([ dayArrayData[0].slice(0, 4), dayArrayData[0].slice(5, 7), dayArrayData[0].slice(8) ]);
+    result[0].unshift(dayArrayData[chartType === 'stock' ? 8 : 1]);
+    result[1].unshift(dayArrayData[chartType === 'stock' ? 9 : 2]);
+    result[2].unshift(dayArrayData[chartType === 'stock' ? 10 : 3]);
+    result[3].unshift(dayArrayData[chartType === 'stock' ? 11 : 4]);
+    return result;
+  }, [ [], [], [], [], [] ]);
+  
+  reformatStockData[5] = dataset;
+  return reformatStockData;
+};
+
+
+// Get data and use it to populate chart
+var buildChart = function (chartType, data) {
+  var fig = PlotlyFinance.createOHLC(
+    {
+      open: data[0],
+      high: data[1],
+      low: data[2],
+      close: data[3],
+      dates: data[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); })
+    }
+  );
+
+  fig.layout.title = chartType === 'stock' ? data[5].name.slice(0, data[5].name.indexOf(')') + 1) : 'S&P 500';
+  fig.layout.xaxis.title = 'Dates';
+  fig.layout.annotations = [{
+    text: chartType === 'stock' ? 'Adjusted Stock Price ($)' : 'Price ($)',
+    x: '-0.075',
+    y: 0.5,
+    xref: 'paper',
+    yref: 'paper',
+    font: {
+      size: 14
+    },
+    showarrow: false,
+    xanchor: 'right',
+    textangle: 270
+  }];
+  
+  Plotly.newPlot('ohlcChart', fig.data, fig.layout);                 
+}
+
+
+// Define the http request function that gets data from argument url
+var getData = function (chartType, url) {
+  httpRequest = new XMLHttpRequest();
           
-  if (stockName) {
-     makeRequest(stockURL);
+  if (!httpRequest) {
+    console.log('Unable to create an XMLHTTP instance.');
+    return false;
+  }
+  
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+      var responseData = JSON.parse(httpRequest.responseText).dataset;
+      var formattedData = formatData(chartType, responseData);
+      buildChart(chartType, formattedData);
+      
+    }
+  };
+  
+  httpRequest.open('GET', url);
+  httpRequest.send();
+}
+
+
+// Define function to execute everything
+var magic = function(chartType) {
+  var stock;
+  var data;
+  if (chartType === 'stock') {
+    stock = getStockURL();
+    data = getData(chartType, stock);
+  } else {
+    data = getData(chartType, 'https://www.quandl.com/api/v3/datasets/YAHOO/INDEX_GSPC.json?api_key=fyWKH12nMF4VuWFaXARN&limit=100');
   }
 };
 
+/* ======================================================================== */
+/* DEFINE ACTIONS */
+/* ======================================================================== */
+
+// Build initial indices
+magic('index');
+
 // On click, make request to get data for stock named in textbox      
-document.getElementById('tickerSubmitButton').onclick = getStock;
+document.getElementById('tickerSubmitButton').onclick = function() {
+  magic('stock');
+};
 
 // On keydown, make request to get data for stock named in textbox
 document.getElementById('tickerTextbox').onkeydown = function() {
   if(event.keyCode === 13) {
-    getStock();
+    magic('stock');
   }
 };
-  
-// Define the http request function that gets data from argument url
-function makeRequest(url) {
-  httpRequest = new XMLHttpRequest();
-          
-  if (!httpRequest) {
-    alert('Unable to create an XMLHTTP instance.');
-    return false;
-  }
-          
-  httpRequest.onreadystatechange = buildChart;
-  httpRequest.open('GET', url);
-  httpRequest.send();
-}
-  
-// Get data and use it to populate chart
-function buildChart() {
-  try {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        console.log(JSON.parse(httpRequest.responseText));
-        var response = JSON.parse(httpRequest.responseText).dataset.data;
-
-        // Reformat Data
-        var reformatData = response.reduce(function(result, dayArrayData) {
-          result[4].unshift([ dayArrayData[0].slice(0, 4), dayArrayData[0].slice(5, 7), dayArrayData[0].slice(8) ]);
-          result[0].unshift(dayArrayData[1]);
-          result[1].unshift(dayArrayData[2]);
-          result[2].unshift(dayArrayData[3]);
-          result[3].unshift(dayArrayData[4]);
-          return result;
-        }, [ [], [], [], [], [] ]);
-                
-        // Create Plotly OHLC Chart
-        var fig = PlotlyFinance.createOHLC(
-          {
-            open: reformatData[0],
-            high: reformatData[1],
-            low: reformatData[2],
-            close: reformatData[3],
-            dates: reformatData[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); })
-          }
-        );
-        Plotly.newPlot('ohlcChart', fig.data, fig.layout);
-                
-      } else {
-        alert('There was a problem with the request.');
-      }
-    }
-  }
-  catch( e ) {
-    alert('Caught Exception: ' + e.description);
-  }
-}
