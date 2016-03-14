16aa3ca99ae1a502d7483440873482c87872a268
michaeljstromberg <michaeljstromberg@gmail.com>
2016-03-14 02:50:19 -0400

    Added volume chart, adjusted colors, converted to weekly. Dates needs to be fixed.

--- assets/javascripts/buildChart.js
+++ assets/javascripts/buildChart.js
@@ -2,12 +2,10 @@
 /* DEFINE FUNCTIONS */
 /* ======================================================================== */
 
-var httpRequest;
-
 // Define function to get ticker symbol and make http reqest
 var getStockURL = function() {
   var stockName = document.getElementById('tickerTextbox').value.toString().toUpperCase();
-  var stockURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?api_key=fyWKH12nMF4VuWFaXARN&limit=100';
+  var stockURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stockName + '.json?api_key=fyWKH12nMF4VuWFaXARN&limit=100&collapse=weekly';
 
   return stockURL;
 };
@@ -16,23 +14,27 @@ var getStockURL = function() {
 // Define function to format received data
 var formatData = function(chartType, dataset) {
   var reformatStockData = dataset.data.reduce(function(result, dayArrayData) {
-    // result = [ adjOpen, adjHigh, adjLow, adjClose, date]
+    // result = [ adjOpen, adjHigh, adjLow, adjClose, date, volume]
     result[4].unshift([ dayArrayData[0].slice(0, 4), dayArrayData[0].slice(5, 7), dayArrayData[0].slice(8) ]);
     result[0].unshift(dayArrayData[chartType === 'stock' ? 8 : 1]);
     result[1].unshift(dayArrayData[chartType === 'stock' ? 9 : 2]);
     result[2].unshift(dayArrayData[chartType === 'stock' ? 10 : 3]);
     result[3].unshift(dayArrayData[chartType === 'stock' ? 11 : 4]);
+    result[5].unshift(dayArrayData[5]);
     return result;
-  }, [ [], [], [], [], [] ]);
+  }, [ [], [], [], [], [], [] ]);
   
-  reformatStockData[5] = dataset;
+  // SHIFT THIS OVER
+  reformatStockData[6] = dataset;
   return reformatStockData;
 };
 
 
 // Get data and use it to populate chart
 var buildChart = function (chartType, data) {
-  var fig = PlotlyFinance.createOHLC(
+  console.log(data);
+  // Build stock chart
+  var stockFig = PlotlyFinance.createOHLC(
     {
       open: data[0],
       high: data[1],
@@ -41,24 +43,60 @@ var buildChart = function (chartType, data) {
       dates: data[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); })
     }
   );
-
-  fig.layout.title = chartType === 'stock' ? data[5].name.slice(0, data[5].name.indexOf(')') + 1) : 'S&P 500';
-  fig.layout.xaxis.title = 'Dates';
-  fig.layout.annotations = [{
-    text: chartType === 'stock' ? 'Adjusted Stock Price ($)' : 'Price ($)',
-    x: '-0.075',
-    y: 0.5,
-    xref: 'paper',
-    yref: 'paper',
-    font: {
-      size: 14
-    },
-    showarrow: false,
-    xanchor: 'right',
-    textangle: 270
-  }];
+  console.log(data[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); }));
+  // Handle layout
+  stockFig.layout = {
+    title: chartType === 'stock' ? data[6].name.slice(0, data[6].name.indexOf(')') + 1) + ' - Weekly': 'S&P 500 - Daily',
+    xaxis: {title: 'Dates'},
+    annotations: [{
+      text: chartType === 'stock' ? 'Adjusted Stock Price ($)     ' : 'Price ($)     ',
+      x: '-0.05',
+      y: 0.5,
+      xref: 'paper',
+      yref: 'paper',
+      font: {
+        size: 14
+      },
+      showarrow: false,
+      xanchor: 'right',
+      textangle: 270
+    }]
+  };
   
-  Plotly.newPlot('ohlcChart', fig.data, fig.layout);                 
+  Plotly.newPlot('ohlcChart', stockFig.data, stockFig.layout);
+  
+  // Build volume chart
+  var volumeData =[];
+  
+  // Build dataset for up weeks
+  volumeData[0] = {
+    x: data[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); }),
+    y: data[5].map(function(vol, index) {
+      return data[3][index] >= data[0][index] ? data[5][index] : 0;
+    }),
+    type: 'bar',
+    marker: { color: 'rgb(93,170,136)' },
+    showlegend: false
+  };
+  
+  // Build dataset for down weeks
+  volumeData[1] = {
+    x: data[4].map(function(d) { return new Date(d[0], d[1]-1, d[2]); }),
+    y: data[5].map(function(vol, index) {
+      return data[3][index] < data[0][index] ? data[5][index] : 0;
+    }),
+    type: 'bar',
+    marker: { color: 'rgb(255,121,113)' },
+    showlegend: false
+  };
+  
+  // Handle layout
+  var layout = {
+    margin: {b: 20, t: 10},
+    yaxis: {title: 'Volume'}
+  };
+   
+  Plotly.newPlot('volumeChart', volumeData, layout);
 }
 
 
@@ -76,7 +114,6 @@ var getData = function (chartType, url) {
       var responseData = JSON.parse(httpRequest.responseText).dataset;
       var formattedData = formatData(chartType, responseData);
       buildChart(chartType, formattedData);
-      
     }
   };
   
